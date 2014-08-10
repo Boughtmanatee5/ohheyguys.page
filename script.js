9f65fda6c718f657866e9ac3256264ae182be496
Sean Costello <sean@nex9.com>
2014-08-10 18:35:59 -0400

    moved extra html to templates

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ mainSection = document.querySelector('.main')
 maleButton = document.querySelector('.male');
 femaleButton = document.querySelector('.female')
 
+loadingHtml = document.getElementById('loading').innerHTML
+
+profileHtml = document.getElementById('profile').innerHTML
+
 maleButton.addEventListener('click', function() {
-  mainSection.innerHTML = "<div class'spinner'>I Spin<div><div class='names'><h2>Bryan Adams</h2></div>";
+  mainSection.innerHTML = loadingHtml;
   window.setTimeout(function() {
-    mainSection.innerHTML = "<h2>ASS BUTT</h2>"
-  }, 2000);
+    mainSection.innerHTML = profileHtml
+  }, 5000);
 })
 
 femaleButton.addEventListener('click', function() {
-  mainSection.innerHTML = "<div class'spinner'>I Spin<div><div class='names'><h2>Bryan Adams</h2></div>";
+  mainSection.innerHTML = loadingHtml;
   window.setTimeout(function() {
-    mainSection.innerHTML = "<h2>ASS BUTT</h2>"
-  }, 2000);
+    mainSection.innerHTML = profileHtml
+  }, 5000);
 })
