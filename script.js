69a316ea738c4cd4d06bf5bbabef2f658610b18c
Sean Costello <sean@nex9.com>
2014-08-12 13:06:50 -0400

    cleaned up some of the javascript

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
-'use-strict';
-var mainSection = document.querySelector('.main');
-var maleButton = document.querySelector('.male');
-var femaleButton = document.querySelector('.female');
+(function () {
+  'use strict';
+  var mainSection = document.querySelector('.main');
+  var maleButton = document.querySelector('.male');
+  var femaleButton = document.querySelector('.female');
 
-var loadingGuysHtml = document.getElementById('loadingGuys').innerHTML;
-var loadingGalsHtml = document.getElementById('loadingGals').innerHTML;
+  var loadingGuysHtml = document.getElementById('loadingGuys').innerHTML;
+  var loadingGalsHtml = document.getElementById('loadingGals').innerHTML;
 
-var profileHtml = document.getElementById('profile').innerHTML;
+  var profileHtml = document.getElementById('profile').innerHTML;
 
-maleButton.addEventListener('click', function() {
-  mainSection.innerHTML = loadingGalsHtml;
+  maleButton.addEventListener('click', function() {
+    mainSection.innerHTML = loadingGalsHtml;
 
-  var namesSection = document.querySelector('.names').childNodes;
-  var i = 1;
-  function switchNames() {
-    namesSection[i].className = '';
-    i+= 2;
-    namesSection[i].className = 'active';
+    var namesSection = document.querySelector('.names').childNodes;
+    var i = 1;
+    function switchNames() {
+      namesSection[i].className = '';
+      i+= 2;
+      namesSection[i].className = 'active';
 
-    if(i <= namesSection.length)  {
-      window.setTimeout(switchNames, 500);
+      if(i <= namesSection.length)  {
+        window.setTimeout(switchNames, 500);
+      }
     }
-  }
-  switchNames();
-  window.setTimeout(function() {
-    mainSection.innerHTML = profileHtml;
-  }, 5000);
-});
+    switchNames();
+    window.setTimeout(function() {
+      mainSection.innerHTML = profileHtml;
+    }, 5000);
+  });
 
-femaleButton.addEventListener('click', function() {
-  mainSection.innerHTML = loadingGuysHtml;
+  femaleButton.addEventListener('click', function() {
+    mainSection.innerHTML = loadingGuysHtml;
 
-  var namesSection = document.querySelector('.names').childNodes;
-  var i = 1;
-  function switchNames() {
-    namesSection[i].className = '';
-    i+= 2;
-    namesSection[i].className = 'active';
+    var namesSection = document.querySelector('.names').childNodes;
+    var i = 1;
+    function switchNames() {
+      namesSection[i].className = '';
+      i+= 2;
+      namesSection[i].className = 'active';
 
-    if(i <= namesSection.length)  {
-      window.setTimeout(switchNames, 500);
+      if(i <= namesSection.length)  {
+        window.setTimeout(switchNames, 500);
+      }
     }
-  }
-  switchNames();
-  window.setTimeout(function() {
-    mainSection.innerHTML = profileHtml;
-  }, 5000);
-});
+    switchNames();
+    window.setTimeout(function() {
+      mainSection.innerHTML = profileHtml;
+    }, 5000);
+  });
+}());
